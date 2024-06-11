6e5d5fa556bca8a4f7ef4290a8becfa290e3210d
Güney Özcan <guoeit00@hs-esslingen.de>
2024-06-11 22:46:22 +0200

    final push. fixed some stuff.

--- src/main/java/swa/labor/SocialNetwork/config/SecurityConfig.java
+++ src/main/java/swa/labor/SocialNetwork/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -26,7 +27,7 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/**", "/users/register", "/users/login", "/posts/**", "/comments/**","/votes/**").permitAll()
